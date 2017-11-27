da4013d7491e55400dae175d2a75103c3df56602
THUONG\Thuong <thuongtruong2702@gmail.com>
2017-11-27 09:04:53 +0700

    add constant

--- app/src/main/java/com/thuong/tu/chatapplication/yolo/utils/Constant.java
+++ app/src/main/java/com/thuong/tu/chatapplication/yolo/utils/Constant.java
@@ -2,7 +2,7 @@ package com.thuong.tu.chatapplication.yolo.utils;
 
 public class Constant {
 
-    public static final String M_HOST = "http://192.168.1.69";
+    public static final String M_HOST = "http://192.168.100.5";
     public static final String M_SERVER_PORT = "3000";
 
     //region link PHP
@@ -10,19 +10,22 @@ public class Constant {
     public static final String M_SIGN_IN = "/chat/login.php";
 
     public static final String M_FRIEND = "/chat/friends.php";
-    public static final String M_FRIEND_ADD = "/chat/friends_add.php";
-    public static final String M_FRIEND_REMOVE = "/chat/friends_remove.php";
+    public static final String M_FRIEND_ADD = "/chat/friend_add.php";
+    public static final String M_FRIEND_REMOVE = "/chat/friend_remove.php";
 
     public static final String M_CONVERSATION = "/chat/conversations.php";
     public static final String M_CONVERSATION_ADD = "/chat/conversation_add.php";
     public static final String M_CONVERSATION_EDIT = "/chat/conversation_update.php";
+    public static final String M_CONVERSATION_SINGLE = "/chat/conversation_single.php";
 
     public static final String M_MESSAGE = "/chat/messages.php";
     public static final String M_MESSAGE_ADD = "/chat/messages_add.php";
     public static final String M_MESSAGE_REMOVE = "/chat/messages_remove.php";
     public static final String M_MESSAGE_EDIT = "/chat/messages_update.php";
+    public static final String M_MESSAGE_IN_CONVERSATION = "/chat/message_in_conversation.php";
 
     public static final String M_INVITE_FRIEND = "/chat/invite_friend.php";
+    public static final String M_USERS = "/chat/users.php";
 
     //endregion link PHP
 }
